e4089b9c8715979c22683366d54ae477070a081a
Daniel Arias <daarrues@gmail.com>
2019-05-02 16:36:48 +0200

    Orden en .h

--- MatLabUtils/MatLabUtils.h
+++ MatLabUtils/MatLabUtils.h
@@ -1,8 +1,18 @@
 #ifndef MATLABUTILS_H
 #define MATLABUTILS_H
 
-double length(double*);
-double dot(double*,double*,int);
 double norm(double*);
+double dot(double*,double*,int);
+// cross (producto vectorial)
+double length(double*);
+// zeros 		(inicializar a 0)
+// * 		(producto de matrices)
+// ‘ 		(traspuesta)
+// det 		(determinante)
+// roots		(raíces de polinomio)
+// unit		(normalizar un vector)
+// isreal		(pertenencia a los reales)
+// sign		(signo)
+// fix
 
 #endif
