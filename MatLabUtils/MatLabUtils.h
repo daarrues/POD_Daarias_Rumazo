3df2fe095d690fae010a3c55eb9b15890368e0d6
daarrues <daarrues@gmail.com>
2019-05-07 01:13:19 +0200

    Arreglado, no sé como se quedó el cambio mal...

--- MatLabUtils/MatLabUtils.h
+++ MatLabUtils/MatLabUtils.h
@@ -7,13 +7,8 @@ void cross(double[3], double[3], double[3]);
 double length(double*);
 void zeros(double[][], int rows, int cols);
 void prodMatr(double[3][3], double[3][3], double[3][3]);
-<<<<<<< HEAD
-// ‘ 		(traspuesta)
-double det(double[3][3]);
-=======
 void trans(double[3][3], double[3][3]);
-// det 		(determinante)
->>>>>>> a52bb767695ab5d48a0007ca782ca96ccdbda66d
+double det(double[3][3]);
 // roots		(raíces de polinomio)
 void unit(double[3]);
 // isreal		(pertenencia a los reales) // <-- QUIZAS NO HAGA FALTA POR ROOTS?
