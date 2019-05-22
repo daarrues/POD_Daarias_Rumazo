2d11f35df482e4965358307c403583e5d4390895
Daniel Arias <daarrues@gmail.com>
2019-05-22 16:24:31 +0200

    Daniel. Fin clase 22-05. (-_-)"

--- lambert_gooding.c
+++ lambert_gooding.c
@@ -61,14 +61,18 @@ void lambert_gooding(double r1[], double r2[], double tof, double long_way,
   }
 
   // initialize:
-  int solution_exists = 0;
+  double max = 2*multi_revs+1;
+  double solution_exists[max];
+  zeros(solution_exists, max);
+  double all_vt1[3][max], all_vt2[3][max];
   double dr       = r1mag - r2mag;
   double r1r2     = r1mag*r2mag;
   double r1hat[3] = unit(r1);
   double r2hat[3] = unit(r2);
   double r1xr2    = cross(r1,r2);
-  if (all(r1xr2==0.0)) // the vectors are parallel,
-  {                    // so the transfer plane is undefined
+
+  if (all(r1xr2)) // vectors are parallel, so the transfer plane is undefined
+  {
       r1xr2 = {0.0, 0.0, 1.0}; // degenerate conic...choose the x-y plane
   }
   double r1xr2_hat[3] = unit(r1xr2);
@@ -123,27 +127,36 @@ void lambert_gooding(double r1[], double r2[], double tof, double long_way,
 
       if (i == 0 && n == 1) // there can be only one solution
       {
-          all_vt1(:,1) = vt1(:,1);
-          all_vt2(:,1) = vt2(:,1);
-          solution_exists(1) = true;
+        for(int j = 0; j < 3; j++)
+        {
+          all_vt1[j][0] = vt1[j][0];
+          all_vt2[j][0] = vt2[j][0];
+        }
+          solution_exists[1] = 1.0;
       }
       else
       {
           switch (n)
           {
               case 1:
-                  all_vt1(:,2*i)         = vt1(:,1);
-                  all_vt2(:,2*i)         = vt2(:,1);
-                  solution_exists(2*i)   = true;
-                  break;
+                for(int j = 0; j < 3; j++)
+                {
+                  all_vt1[j][2*i-1] = vt1[j][0];
+                  all_vt2[j][2*i-1] = vt2[j][0];
+                }
+                solution_exists[2*i-1]   = 1.0;
+                break;
               case 2:
-                  all_vt1(:,2*i)         = vt1(:,1);
-                  all_vt2(:,2*i)         = vt2(:,1);
-                  solution_exists(2*i)   = true;
-                  all_vt1(:,2*i+1)       = vt1(:,2);
-                  all_vt2(:,2*i+1)       = vt2(:,2);
-                  solution_exists(2*i+1) = true;
-                  break;
+                for(int j = 0; j < 3; j++)
+                {
+                  all_vt1[j][2*i-1] = vt1[j][0];
+                  all_vt2[j][2*i-1] = vt2[j][0];
+                  all_vt1[j][2*i] = vt1[j][1];
+                  all_vt2[j][2*i] = vt2[j][1];
+                }
+                solution_exists[2*i-1]  = 1.0;
+                solution_exists[2*i]    = 1.0;
+                break;
           }
       }
   }
@@ -199,8 +212,8 @@ void vlamb(double gm, double r1, double r2, double th, double tdelt, double n,
  * @return the 8th root of x
  */
 //------------------------------------------------------------------------------
-void tlamb(double m, double q, double qsqfm1, double x, double n, double t,
-           double dt, double d2t, double d3t)
+void tlamb(double m, double q, double qsqfm1, double x, double n, double *t,
+           double *dt, double *d2t, double *d3t)
 {
   double y, z, qx, a, b, aa, bb, g, f, fg1, term, fg1sq, twoi1, told, qz, qz2,
          u0i, u1i, u2i, u3i, tq, i, tqsum, ttmold, p, tterm, tqterm;
@@ -220,9 +233,9 @@ void tlamb(double m, double q, double qsqfm1, double x, double n, double t,
   if (!lm1)
   {
       // (needed if series, and otherwise useful when z = 0)
-      dt = 0.0;
-      d2t = 0.0;
-      d3t = 0.0;
+      *dt = 0.0;
+      *d2t = 0.0;
+      *d3t = 0.0;
   }
 
   if (lm1 || m > 0 || x < 0.0 || fabs(u) > sw)
@@ -265,28 +278,28 @@ void tlamb(double m, double q, double qsqfm1, double x, double n, double t,
       f = a*y;
       if (x <= 1.0)
       {
-        t = m*M_PI + atan2(f, g);
+        *t = m*M_PI + atan2(f, g);
       }
       else
       {
         if (f > sw)
         {
-            t = log(f + g);
+            *t = log(f + g);
         }
         else
         {
           fg1 = f/(g + 1.0);
           term = 2.0*fg1;
           fg1sq = fg1*fg1;
-          t = term;
+          *t = term;
           twoi1 = 1.0;
           while(1)
           {
             twoi1 = twoi1 + 2.0;
             term = term*fg1sq;
-            told = t;
-            t = t + term/twoi1;
-            if (t != told)
+            told = *t;
+            *t = *t + term/twoi1;
+            if (*t != told)
             {
               // cycle
               break;
@@ -294,28 +307,28 @@ void tlamb(double m, double q, double qsqfm1, double x, double n, double t,
           }// (continue looping for inverse tanh)
         }
       }
-      t = 2.0*(t/y + b)/u;
+      *t = 2.0*((*t)/y + b)/u;
       if (l1 && z != 0.0)
       {
         qz = q/z;
         qz2 = qz*qz;
         qz = qz*qz2;
-        dt = (3.0*x*t - 4.0*(a + qx*qsqfm1)/z)/u;
+        *dt = (3.0*x*(*t) - 4.0*(a + qx*qsqfm1)/z)/u;
         if (l2)
         {
-            d2t = (3.0*t + 5.0*x*dt + 4.0*qz*qsqfm1)/u;
+            *d2t = (3.0*(*t) + 5.0*x*(*dt) + 4.0*qz*qsqfm1)/u;
         }
         if (l3)
         {
-            d3t = (8.0*dt + 7.0*x*d2t - 12.0*qz*qz2*x*qsqfm1)/u;
+            *d3t = (8.0*dt + 7.0*x*(*d2t) - 12.0*qz*qz2*x*qsqfm1)/u;
         }
       }
     }
     else
     {
-      dt = b;
-      d2t = bb;
-      d3t = aa;
+      *dt = b;
+      *d2t = bb;
+      *d3t = aa;
     }
   }
   else
@@ -346,7 +359,7 @@ void tlamb(double m, double q, double qsqfm1, double x, double n, double t,
         tqsum = (1.0/(1.0 + q) + q)*qsqfm1;
       }
       ttmold = term/3.0;
-      t = ttmold*tqsum;
+      *t = ttmold*tqsum;
       while(1)
       {
         i = i + 1;
@@ -367,25 +380,25 @@ void tlamb(double m, double q, double qsqfm1, double x, double n, double t,
         term = term*(p - 0.5)/p;
         tq = tq*qsq;
         tqsum = tqsum + tq;
-        told = t;
+        told = *t;
         tterm = term/(2.0*p + 3.0);
         tqterm = tterm*tqsum;
-        t = t - u0i*((1.5*p + 0.25)*tqterm/(p*p - 0.25)-ttmold*tq);
+        *t = *t - u0i*((1.5*p + 0.25)*tqterm/(p*p - 0.25)-ttmold*tq);
         ttmold = tterm;
         tqterm = tqterm*p;
         if (l1)
         {
-          dt = dt + tqterm*u1i;
+          *dt = *dt + tqterm*u1i;
         }
         if (l2)
         {
-          d2t = d2t + tqterm*u2i*(p - 1.0);
+          *d2t = *d2t + tqterm*u2i*(p - 1.0);
         }
         if (l3)
         {
-          d3t = d3t + tqterm*u3i*(p - 1.0)*(p - 2.0);
+          *d3t = *d3t + tqterm*u3i*(p - 1.0)*(p - 2.0);
         }
-        if (i < n || t != told)
+        if (i < n || *t != told)
         {
           // cycle
           break;
@@ -393,17 +406,17 @@ void tlamb(double m, double q, double qsqfm1, double x, double n, double t,
       }
     if (l3)
     {
-      d3t = 8.0*x*(1.5*d2t - xsq*d3t);
+      *d3t = 8.0*x*(1.5*(*d2t) - xsq*(*d3t));
     }
     if (l2)
     {
-      d2t = 2.0*(2.0*xsq*d2t - dt);
+      *d2t = 2.0*(2.0*xsq*(*d2t) - (*dt));
     }
     if (l1)
     {
-      dt = -2.0*x*dt;
+      *dt = -2.0*x*(*dt);
     }
-    t = t/xsq;
+    *t = *t/xsq;
   }
 }
 //------------------------------------------------------------------------------
@@ -437,9 +450,192 @@ double d8rt (double x)
  * @return the 8th root of x
  */
 //------------------------------------------------------------------------------
-void xlamb(double m, double q, double qsqfm1, double tin, double n, double x,
-           double xpl)
+void xlamb(double m, double q, double qsqfm1, double tin, double *n, double *x,
+           double *xpl)
 {
+  // Gooding support routine
+  double tmin, dt, d2t, d3t, t0, t;
 
+  double tol = 3e-7;
+  double c0  = 1.7;
+  double c1  = 0.5;
+  double c2  = 0.03;
+  double c3  = 0.15;
+  double c41 = 1.0;
+  double c42 = 0.24;
+
+  double thr2 = atan2(qsqfm1, 2.0*q)/M_PI;
+
+  *xpl = 0;
+  *x = 0;
+
+  if (m == 0)
+  {
+      // single-rev starter from t (at x = 0) & bilinear (usually)
+      *n = 1;
+      tlamb(m, q, qsqfm1, 0, 0, &t0, &dt, &d2t, &d3t);
+      tdiff = tin - t0;
+      if (tdiff <= 0.0)
+      {
+          *x = t0*tdiff/(-4.0*tin);
+          // (-4 is the value of dt, for x = 0)
+      }
+      else
+      {
+          x = -tdiff/(tdiff + 4.0);
+          w = *x + c0*sqrt(2.0*(1.0 - thr2));
+          if (w < 0.0)
+          {
+            *x = *x - sqrt(d8rt(-w))*(x + sqrt(tdiff/(tdiff + 1.5*t0)));
+          }
+          w = 4.0/(4.0 + tdiff);
+          *x = (*x)*(1.0 + (*x)*(c1*w - c2*(*x)*sqrt(w)));
+      }
+  }
+  else
+  {
+    // with multirevs, first get t(min) as basis for starter
+    xm = 1.0/(1.5*(m + 0.5)*M_PI);
+    if (thr2 < 0.5)
+    {
+          xm = d8rt(2.0*thr2)*xm;
+      }
+    if (thr2 > 0.5)
+    {
+          xm = (2.0 - d8rt(2.0 - 2.0*thr2))*xm;
+      }
+    // (starter for tmin)
+
+    for (int i = 0; i < 12; i++)
+    {
+          tlamb(m, q, qsqfm1, xm, 3, &tmin, &dt, &d2t, &d3t);
+          if (d2t == 0.0)
+          {
+              break;
+          }
+          xmold = xm;
+          xm = xm - dt*d2t/(d2t*d2t - dt*d3t/2.0);
+          xtest = abs(xmold/xm - 1.0);
+          if (xtest <= tol)
+          {
+              break;
+          }
+      }
+
+    if (i>12)
+    {
+          // (break off & exit if tmin not located - should never happen)
+          // now proceed from t(min) to full starter
+          n = -1;
+          return;
+      }
+    tdiffm = tin - tmin;
+    if (tdiffm < 0.0)
+    {
+          *n = 0;
+          return;
+          // (exit if no solution with this m)
+      }
+    else if (tdiffm == 0.0)
+    {
+          *x = xm;
+          *n = 1;
+          return;
+          // (exit if unique solution already from x(tmin))
+      }
+    else
+    {
+        *n = 3;
+        if (d2t == 0.0)
+        {
+              d2t = 6.0*m*M_PI;
+        }
+        *x = sqrt(tdiffm/(d2t/2.0 + tdiffm/(1.0 - xm)^2));
+        w = xm + *x;
+        w = w*4.0/(4.0 + tdiffm) + (1.0 - w)^2;
+        *x = (*x)*(1.0 - (1.0 + m + c41*(thr2 - 0.5))/(1.0 + c3*m)*(*x)*(c1*w + c2*(*x)*sqrt(w))) + xm;
+        d2t2 = d2t/2.0;
+        if (*x >= 1.0)
+        {
+          *n = 1;
+          // goto 3
+          tlamb(m, q, qsqfm1, 0.0 , 0, &t0, &dt, &d2t, &d3t); // 3
+          tdiff0 = t0 - tmin;
+          tdiff = tin - t0;
+          if (tdiff <= 0)
+          {
+            *x = xm - sqrt(tdiffm/(d2t2 - tdiffm*(d2t2/tdiff0 - 1.0/pow(xm,2))));
+          }
+          else
+          {
+              *x = -tdiff/(tdiff + 4.0);
+              ij = 200;
+              w = *x + c0*sqrt(2.0*(1.0 - thr2));
+              if (w<0.0)
+              {
+                *x = *x - sqrt(d8rt(-w))*((*x) + sqrt(tdiff/(tdiff+1.5*t0)));
+              }
+              w = 4.0/(4.0 + tdiff);
+              *x = (*x)*(1.0 + (1.0 + m + c42*(thr2 - 0.5))/(1.0 + c3*m)*(*x)*(c1*w - c2*(*x)*sqrt(w)));
+              if (*x <= -1.0)
+              {
+                *n = *n - 1;
+                // (no finite solution with x < xm)
+                if (*n == 1)
+                {
+                    *x = *xpl;
+                }
+              }
+          } // 3
+        }
+        // (no finite solution with x > xm)
+      }
+  }
+  // (now have a starter, so proceed by halley)
+      for (int i = 0; i < 3; i++)
+      {
+          [t,dt,d2t,d3t] = tlamb(m,q,qsqfm1,*x,2);
+          t = tin - t;
+          if (dt != 0.0)
+          {
+              *x = *x + t*dt/(dt*dt + t*d2t/2.0);
+          }
+      }
+      if (n != 3)
+      {
+          return
+      }
+      // (exit if only one solution, normally when m = 0)
+      *n = 2;
+      *xpl = *x;
+      // (second multi-rev starter)
+      tlamb(m, q, qsqfm1, 0.0 , 0, &t0, &dt, &d2t, &d3t); // 3
+      tdiff0 = t0 - tmin;
+      tdiff = tin - t0;
+      if (tdiff<=0)
+      {
+          *x = xm - sqrt(tdiffm/(d2t2 - tdiffm*(d2t2/tdiff0 - 1.0/xm^2)));
+      }
+      else
+      {
+          *x = -tdiff/(tdiff + 4.0);
+          ij = 200;
+          w = *x + c0*sqrt(2.0*(1.0 - thr2));
+          if (w < 0.0)
+          {
+              *x = *x - sqrt(d8rt(-w))*((*x) + sqrt(tdiff/(tdiff+1.5*t0)));
+          }
+          w = 4.0/(4.0 + tdiff);
+          *x = (*x)*(1.0 + (1.0 + m + c42*(thr2 - 0.5))/(1.0 + c3*m)*(*x)*(c1*w - c2*(*x)*sqrt(w)));
+          if (*x <= -1.0)
+          {
+              *n = *n - 1;
+              // (no finite solution with x < xm)
+              if (*n == 1)
+              {
+                  *x = xpl;
+              }
+          }
+      }
 }
 //------------------------------------------------------------------------------
