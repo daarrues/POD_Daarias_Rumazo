5d510ca5bbf01939eb18f7a0d0baeec99b741745
rumazo <rumazo@unirioja.es>
2019-05-25 22:02:40 +0200

    Rubén. Mejora de los test (precisión)

--- TestRumazo.c
+++ TestRumazo.c
@@ -27,8 +27,8 @@ typedef int bool;
 //------------------------------------------------------------------------------
 void testMeanObliquity1(bool verbose)
 {
-	double n = 54977.6676696643;
-	double esperado = 0.409071470558628;
+	double n = 54977.66766966425300000000;
+	double esperado = 0.40907147055862825000;
 	double obtenido = MeanObliquity(n);
 
 	if(verbose)
@@ -53,8 +53,8 @@ void testMeanObliquity1(bool verbose)
 //------------------------------------------------------------------------------
 void testMeanObliquity2(bool verbose)
 {
-	double n = 55565.5429708796;
-	double esperado = 0.409067817509821;
+	double n = 55565.90517337957900000000;
+	double esperado = 0.40906781525909947000;
 	double obtenido = MeanObliquity(n);
 
 	if(verbose)
@@ -79,8 +79,8 @@ void testMeanObliquity2(bool verbose)
 //------------------------------------------------------------------------------
 void testMeanObliquity3(bool verbose)
 {
-	double n = 55565.9051733796;
-	double esperado = 0.409067815259099;
+	double n = 54332.48686555545300000000;
+	double esperado = 0.40907547970278441000;
 	double obtenido = MeanObliquity(n);
 
 	if(verbose)
@@ -105,9 +105,9 @@ void testMeanObliquity3(bool verbose)
 //------------------------------------------------------------------------------
 void testNutAngles1(bool verbose)
 {
-	double n = 54977.6676696643;
-	double esperado1 = 6.48693387393849e-05;
-	double esperado2 = 2.23051336707266e-05;
+	double n = 54977.66766966425300000000;
+	double esperado1 = 0.00006486933873938495;
+	double esperado2 = 0.00002230513367072657;
 	double obtenido1;
 	double obtenido2;
 
@@ -138,9 +138,9 @@ void testNutAngles1(bool verbose)
 //------------------------------------------------------------------------------
 void testNutAngles2(bool verbose)
 {
-	double n = 53989.1991812154;
-	double esperado1 = 7.09833624503762e-06;
-	double esperado2 = 4.67343559525024e-05;
+	double n = 55565.90517337957900000000;
+	double esperado1 = 0.00008742355142398978;
+	double esperado2 = -0.00000075329307704148;
 	double obtenido1;
 	double obtenido2;
 
@@ -171,9 +171,9 @@ void testNutAngles2(bool verbose)
 //------------------------------------------------------------------------------
 void testNutAngles3(bool verbose)
 {
-	double n = 54332.4868655555;
-	double esperado1 = 3.52072380612286e-05;
-	double esperado2 = 4.18260166677631e-05;
+	double n = 54332.48686555545300000000;
+	double esperado1 = 0.00003520723806122855;
+	double esperado2 = 0.00004182601666776308;
 	double obtenido1;
 	double obtenido2;
 
@@ -204,8 +204,8 @@ void testNutAngles3(bool verbose)
 //------------------------------------------------------------------------------
 void testEqnEquinox1(bool verbose)
 {
-	double n = 54977.6676696643;
-	double esperado = 5.95170051422054e-05;
+	double n = 54977.66766966426100000000;
+	double esperado = 0.00005951700514220537;
 	double obtenido = EqnEquinox(n);
 
 	if(verbose)
@@ -230,8 +230,8 @@ void testEqnEquinox1(bool verbose)
 //------------------------------------------------------------------------------
 void testEqnEquinox2(bool verbose)
 {
-	double n = 55565.9051733796;
-	double esperado = 8.02104092023363e-05;
+	double n = 55565.90517337957900000000;
+	double esperado = 0.00008021040920233633;
 	double obtenido = EqnEquinox(n);
 
 	if(verbose)
@@ -256,8 +256,8 @@ void testEqnEquinox2(bool verbose)
 //------------------------------------------------------------------------------
 void testEqnEquinox3(bool verbose)
 {
-	double n = 53989.1991812154;
-	double esperado = 6.51263906819133e-06;
+	double n = 54332.48686555545300000000;
+	double esperado = 0.00003230225199490219;
 	double obtenido = EqnEquinox(n);
 
 	if(verbose)
@@ -282,8 +282,8 @@ void testEqnEquinox3(bool verbose)
 //------------------------------------------------------------------------------
 void testFrac1(bool verbose)
 {
-	double n = 10.3456636914056;
-	double esperado = 0.3456636914056;
+	double n = 10.34566369140563100000;
+	double esperado = 0.34566369140563147000;
 	double obtenido = Frac(n);
 
 	if(verbose)
@@ -308,8 +308,8 @@ void testFrac1(bool verbose)
 //------------------------------------------------------------------------------
 void testFrac2(bool verbose)
 {
-	double n = 7.17084861050766;
-	double esperado = 0.17084861050766;
+	double n = 12.19370373739637800000;
+	double esperado = 0.19370373739637792000;
 	double obtenido = Frac(n);
 
 	if(verbose)
@@ -334,8 +334,8 @@ void testFrac2(bool verbose)
 //------------------------------------------------------------------------------
 void testFrac3(bool verbose)
 {
-	double n = 11.2920772238043;
-	double esperado = 0.2920772238043;
+	double n = 8.39841973164080890000;
+	double esperado = 0.39841973164080891000;
 	double obtenido = Frac(n);
 
 	if(verbose)
@@ -360,8 +360,8 @@ void testFrac3(bool verbose)
 //------------------------------------------------------------------------------
 void testGmst1(bool verbose)
 {
-	double n = 54977.6669066321;
-	double esperado = 2.17186902706532;
+	double n = 54977.66690663211200000000;
+	double esperado = 2.17186902706532250000;
 	double obtenido = gmst(n);
 
 	if(verbose)
@@ -371,7 +371,7 @@ void testGmst1(bool verbose)
 		printf("Obtenido: %.20lf \n", obtenido);
 	}
 
-	assert(fabs(esperado - obtenido) < 10e-8);
+	assert(fabs(esperado - obtenido) < 10e-12);
 	printf("gmst1 superado!\n");
 }
 
@@ -386,8 +386,8 @@ void testGmst1(bool verbose)
 //------------------------------------------------------------------------------
 void testGmst2(bool verbose)
 {
-	double n = 55565.9044057253;
-	double esperado = 1.21707647675469;
+	double n = 55565.90440572530500000000;
+	double esperado = 1.21707647675469470000;
 	double obtenido = gmst(n);
 
 	if(verbose)
@@ -397,7 +397,7 @@ void testGmst2(bool verbose)
 		printf("Obtenido: %.20lf \n", obtenido);
 	}
 
-	assert(fabs(esperado - obtenido) < 10e-8);
+	assert(fabs(esperado - obtenido) < 10e-12);
 	printf("gmst2 superado!\n");
 }
 
@@ -412,8 +412,8 @@ void testGmst2(bool verbose)
 //------------------------------------------------------------------------------
 void testGmst3(bool verbose)
 {
-	double n = 54332.4861092138;
-	double esperado = 2.50334500393596;
+	double n = 54332.48610921379400000000;
+	double esperado = 2.50334500393596440000;
 	double obtenido = gmst(n);
 
 	if(verbose)
@@ -423,7 +423,7 @@ void testGmst3(bool verbose)
 		printf("Obtenido: %.20lf \n", obtenido);
 	}
 
-	assert(fabs(esperado - obtenido) < 10e-8);
+	assert(fabs(esperado - obtenido) < 10e-12);
 	printf("gmst3 superado!\n");
 }
 
@@ -438,13 +438,13 @@ void testGmst3(bool verbose)
 //------------------------------------------------------------------------------
 void testTimediff1(bool verbose)
 {
-	double n1 = 0.258022690875596;
-	double n2 = 34.0;
-	double esp1 = -33.7419773091244;
-	double esp2 = -15.0;
-	double esp3 = -14.7419773091244;
-	double esp4 = 66.184;
-	double esp5 = 15.0;
+	double n1 = 0.25802269087559637000;
+	double n2 = 34.00000000000000000000;
+	double esp1 = -33.74197730912440100000;
+	double esp2 = -15.00000000000000000000;
+	double esp3 = -14.74197730912440100000;
+	double esp4 = 66.18399999999999700000;
+	double esp5 = 15.00000000000000000000;
 	double obt1;
 	double obt2;
 	double obt3;
@@ -487,13 +487,13 @@ void testTimediff1(bool verbose)
 //------------------------------------------------------------------------------
 void testTimediff2(bool verbose)
 {
-	double n1 = -0.141248008109364;
-	double n2 = 34.0;
-	double esp1 = -34.1412480081094;
-	double esp2 = -15.0;
-	double esp3 = -15.1412480081094;
-	double esp4 = 66.184;
-	double esp5 = 15.0;
+	double n1 = -0.14132917768961278000;
+	double n2 = 34.00000000000000000000;
+	double esp1 = -34.14132917768961300000;
+	double esp2 = -15.00000000000000000000;
+	double esp3 = -15.14132917768961300000;
+	double esp4 = 66.18399999999999700000;
+	double esp5 = 15.00000000000000000000;
 	double obt1;
 	double obt2;
 	double obt3;
@@ -536,13 +536,13 @@ void testTimediff2(bool verbose)
 //------------------------------------------------------------------------------
 void testTimediff3(bool verbose)
 {
-	double n1 = 0.161894409590604;
-	double n2 = 33.0;
-	double esp1 = -32.8381055904094;
-	double esp2 = -14.0;
-	double esp3 = -13.8381055904094;
-	double esp4 = 65.184;
-	double esp5 = 14.0;
+	double n1 = -0.16391912638890360000;
+	double n2 = 33.00000000000000000000;
+	double esp1 = -33.16391912638890500000;
+	double esp2 = -14.00000000000000000000;
+	double esp3 = -14.16391912638890500000;
+	double esp4 = 65.18399999999999700000;
+	double esp5 = 14.00000000000000000000;
 	double obt1;
 	double obt2;
 	double obt3;
@@ -588,14 +588,14 @@ void testIERS1(bool verbose)
 	double *eopdata[13];
 	leerFichero(eopdata);
 	int n1 = 20026;
-	double n2 = 54977.6669036457;
+	double n2 = 54977.66690364573200000000;
 	char c = 'l';
-	double esp1 = 0.258022690875596;
-	double esp2 = 34.0;
-	double esp3 = 7.5789200806793e-08;
-	double esp4 = 2.56777193042581e-06;
-	double esp5 = -2.91335538497448e-07;
-	double esp6 = -4.54223178651006e-08;
+	double esp1 = 0.25802269087559637000;
+	double esp2 = 34.00000000000000000000;
+	double esp3 = 0.00000007578920080679;
+	double esp4 = 0.00000256777193042581;
+	double esp5 = -0.00000029133553849745;
+	double esp6 = -0.00000004542231786510;
 	double obt1;
 	double obt2;
 	double obt3;
@@ -645,14 +645,14 @@ void testIERS2(bool verbose)
 	double *eopdata[13];
 	leerFichero(eopdata);
 	int n1 = 20026;
-	double n2 = 53989.198426771;
+	double n2 = 55565.90440736105700000000;
 	char c = 'l';
-	double esp1 = 0.161894409590604;
-	double esp2 = 33.0;
-	double esp3 = 3.41489253490265e-07;
-	double esp4 = 1.23201492955798e-06;
-	double esp5 = -3.25133150333998e-07;
-	double esp6 = -2.74651440721866e-08;
+	double esp1 = -0.14132917768961278000;
+	double esp2 = 34.00000000000000000000;
+	double esp3 = 0.00000057222922939284;
+	double esp4 = 0.00000097165542393736;
+	double esp5 = -0.00000032281878992102;
+	double esp6 = -0.00000003031019908952;
 	double obt1;
 	double obt2;
 	double obt3;
@@ -702,14 +702,14 @@ void testIERS3(bool verbose)
 	double *eopdata[13];
 	leerFichero(eopdata);
 	int n1 = 20026;
-	double n2 = 55565.9044073611;
+	double n2 = 54332.48611111100800000000;
 	char c = 'l';
-	double esp1 = -0.141329177689613;
-	double esp2 = 34.0;
-	double esp3 = 5.72229229392844e-07;
-	double esp4 = 9.71655423937364e-07;
-	double esp5 = -3.22818789921021e-07;
-	double esp6 = -3.03101990895201e-08;
+	double esp1 = -0.16391912638890360000;
+	double esp2 = 33.00000000000000000000;
+	double esp3 = 0.00000101984436330847;
+	double esp4 = 0.00000138460955806667;
+	double esp5 = -0.00000032922215709019;
+	double esp6 = -0.00000003203992214460;
 	double obt1;
 	double obt2;
 	double obt3;
@@ -756,8 +756,8 @@ void testIERS3(bool verbose)
 //------------------------------------------------------------------------------
 void testGstime1(bool verbose)
 {
-	double n = 54977.6669066321;
-	double esperado = 2.17192854407046;
+	double n = 54977.66690663211200000000;
+	double esperado = 2.17192854407046450000;
 	double obtenido = gstime(n);
 
 	if(verbose)
@@ -767,7 +767,7 @@ void testGstime1(bool verbose)
 		printf("Obtenido: %.20lf \n", obtenido);
 	}
 
-	assert(fabs(esperado - obtenido) < 10e-8);
+	assert(fabs(esperado - obtenido) < 10e-12);
 	printf("gstime1 superado!\n");
 }
 
@@ -782,8 +782,8 @@ void testGstime1(bool verbose)
 //------------------------------------------------------------------------------
 void testGstime2(bool verbose)
 {
-	double n = 55565.9044057253;
-	double esperado = 1.2171566871639;
+	double n = 55565.90440572530500000000;
+	double esperado = 1.21715668716389700000;
 	double obtenido = gstime(n);
 
 	if(verbose)
@@ -793,7 +793,7 @@ void testGstime2(bool verbose)
 		printf("Obtenido: %.20lf \n", obtenido);
 	}
 
-	assert(fabs(esperado - obtenido) < 10e-8);
+	assert(fabs(esperado - obtenido) < 10e-12);
 	printf("gstime2 superado!\n");
 }
 
@@ -808,8 +808,8 @@ void testGstime2(bool verbose)
 //------------------------------------------------------------------------------
 void testGstime3(bool verbose)
 {
-	double n = 54332.4861092138;
-	double esperado = 2.50337730618796;
+	double n = 54332.48610921379400000000;
+	double esperado = 2.50337730618795940000;
 	double obtenido = gstime(n);
 
 	if(verbose)
@@ -819,7 +819,7 @@ void testGstime3(bool verbose)
 		printf("Obtenido: %.20lf \n", obtenido);
 	}
 
-	assert(fabs(esperado - obtenido) < 10e-8);
+	assert(fabs(esperado - obtenido) < 10e-12);
 	printf("gstime3 superado!\n");
 }
 
