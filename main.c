e966acf16e6cdba9eec6b7e3328fbe0a58b211e4
rumazo <rumazo@unirioja.es>
2019-06-02 02:20:49 +0200

    Rubén. Estructura de ejemplos

---
+++ main.c
@@ -0,0 +1,19 @@
+//------------------------------------------------------------------------------
+//                              Main
+//------------------------------------------------------------------------------
+// POD: Preliminary Orbit Determination
+/**
+ * @file main.c
+ * @author Daniel Arias Ruiz-Esquide y Rubén Mazo Tomás
+ * @date Created: 2019/06/02
+ *
+ * Este fichero contiene la función principal con el menú para ejecutar los
+ * ejemplos de M. Mahooti.
+ */
+//------------------------------------------------------------------------------
+#include "examples.h"
+
+int main()
+{
+  return 0;
+}
